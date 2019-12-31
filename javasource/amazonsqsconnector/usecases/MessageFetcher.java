273881ce31c299d2d4398b3bb6eef58be23a37e0
Menno de Haas <menno.dehaas@webflight.nl>
2019-12-31 15:57:50 +0100

    Added infinite fetching action and examples.

---
+++ javasource/amazonsqsconnector/usecases/MessageFetcher.java
@@ -0,0 +1,29 @@
+package amazonsqsconnector.usecases;
+
+import com.mendix.core.Core;
+import com.mendix.core.CoreException;
+import com.mendix.logging.ILogNode;
+import com.mendix.systemwideinterfaces.core.IContext;
+
+public class MessageFetcher implements Runnable {
+	
+	private ILogNode logger;
+	private String prizeRequestFetchingMicroflow;
+	
+	public MessageFetcher(ILogNode logger, String prizeRequestFetchingMicroflow) {
+		this.logger = logger;
+		this.prizeRequestFetchingMicroflow = prizeRequestFetchingMicroflow;
+	}
+
+	@Override
+	public void run() {
+		while(true) {
+			try {
+				IContext context = Core.createSystemContext();
+				Core.execute(context, prizeRequestFetchingMicroflow);
+			} catch (CoreException e) {
+				logger.error("Error during prize request fetching.", e);
+			}
+		}	
+	}
+}
